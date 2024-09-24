1c5e0a7c3c1cdb649a6ac48e5ed567d2e44e50d9
landsamboni <landsamboni@gmail.com>
2024-09-24 09:33:37 -0500

    configuraciones adicionales NGINX

--- main.tf
+++ main.tf
@@ -19,7 +19,35 @@ resource "kubernetes_config_map" "nginx_config" {
     "searchenginehotels.conf"   = file("${path.module}/nginx-config-files/searchenginehotels.conf")
     "cmsaws.conf"               = file("${path.module}/nginx-config-files/cmsaws.conf")
 
+    # configuraciones adicionales
 
+    "app-miles.conf"              = file("${path.module}/nginx-config-files/app-miles.conf")
+    "app-vjs-argentina.conf"      = file("${path.module}/nginx-config-files/app-vjs-argentina.conf")
+    "app-vjs-bolivia.conf"        = file("${path.module}/nginx-config-files/app-vjs-bolivia.conf")
+    "app-vjs-brasil.conf"         = file("${path.module}/nginx-config-files/app-vjs-brasil.conf")
+    "app-vjs-canada.conf"         = file("${path.module}/nginx-config-files/app-vjs-canada.conf")
+    "app-vjs-chile.conf"          = file("${path.module}/nginx-config-files/app-vjs-chile.conf")
+    "app-vjs-colombia.conf"       = file("${path.module}/nginx-config-files/app-vjs-colombia.conf")
+    "app-vjs-costa-rica.conf"     = file("${path.module}/nginx-config-files/app-vjs-costa-rica.conf")
+    "app-vjs-ecuador.conf"        = file("${path.module}/nginx-config-files/app-vjs-ecuador.conf")
+    "app-vjs-espana.conf"         = file("${path.module}/nginx-config-files/app-vjs-espana.conf")
+    "app-vjs-guatemala.conf"      = file("${path.module}/nginx-config-files/app-vjs-guatemala.conf")
+    "app-vjs-honduras.conf"       = file("${path.module}/nginx-config-files/app-vjs-honduras.conf")
+    "app-vjs-mexico.conf"         = file("${path.module}/nginx-config-files/app-vjs-mexico.conf")
+    "app-vjs-nicaragua.conf"      = file("${path.module}/nginx-config-files/app-vjs-nicaragua.conf")
+    "app-vjs-panama.conf"         = file("${path.module}/nginx-config-files/app-vjs-panama.conf")
+    "app-vjs-paraguay.conf"       = file("${path.module}/nginx-config-files/app-vjs-paraguay.conf")
+    "app-vjs-peru.conf"           = file("${path.module}/nginx-config-files/app-vjs-peru.conf")
+    "app-vjs-portugal.conf"       = file("${path.module}/nginx-config-files/app-vjs-portugal.conf")
+    "app-vjs-puerto-rico.conf"    = file("${path.module}/nginx-config-files/app-vjs-puerto-rico.conf")
+    "app-vjs-reino-unido.conf"    = file("${path.module}/nginx-config-files/app-vjs-reino-unido.conf")
+    "app-vjs-rep-dominicana.conf" = file("${path.module}/nginx-config-files/app-vjs-rep-dominicana.conf")
+    "app-vjs-salvador.conf"       = file("${path.module}/nginx-config-files/app-vjs-salvador.conf")
+    "app-vjs-uruguay.conf"        = file("${path.module}/nginx-config-files/app-vjs-uruguay.conf")
+    "app-vjs-venezuela.conf"      = file("${path.module}/nginx-config-files/app-vjs-venezuela.conf")
+    "leads.conf"                  = file("${path.module}/nginx-config-files/leads.conf")
+    "requests.conf"               = file("${path.module}/nginx-config-files/requests.conf")
+    "unfinish.conf"               = file("${path.module}/nginx-config-files/unfinish.conf")
   }
 }
 
@@ -182,3 +210,4 @@ resource "kubernetes_ingress_v1" "grafana_ingress" {
     }
   }
 }
+
